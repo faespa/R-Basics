810c3fd7aef1d2c54fcc5cd176d02b04fec8b4c9
Faespa <35931428+faespa@users.noreply.github.com>
2022-06-19 00:52:35 -0500

    updates Rbasics
    Arrays and Matrix

--- Rbasics.R
+++ Rbasics.R
@@ -1,5 +1,4 @@
-#Data Frame
-
+#########DATA FRAMES##############
 library(dslabs)
 
 data(murders)
@@ -24,7 +23,6 @@ LenghtPop <- length(murders$population)
 class(LenghtPop)
 LenghtPop
 
-
 #Factores
 class(murders$region)
 levels(murders$region)
@@ -36,18 +34,20 @@ region <- reorder(region, value, FUN = sum)
 levels(region)
 
 
-#Generar Listas
+
+################Listas#################
 # la c se utiliza para crear un vector 
 record <- list(name = "John Doe",
                student_id = 1234,
                grades = c(95, 82, 91, 97, 93),
                final_grade = "A")
+
 #tambien se puede extraer datos de la lista como en los dataFrames 
 record$student_id
 
 
 
-######MATRICES#######
+###########MATRICES#######
 #generar una matriz especificar filas y columnas
 mat <- matrix(1:12, 4, 3)
 #acceder a la matriz
@@ -88,6 +88,60 @@ country <- c("italy","canada","egypt")
 names(codes) <- country
 codes
 
+#cceso a elementos específicos de un vector
+codes[2];
+
+#obtener más de una entrada utilizando un vector de entradas
+codes[c(1,3)]
+
+#Obtener secuencia defeinida
+codes[1:2]
+
+#obtener valor por medio del nombre 
+codes["canada"]
+codes[c("egypt","italy")]
 
 
 
+#######SECUENCIAS###########
+#son numeros enteros no numericos usados para indexar
+class(1:10)
+
+#si creamos una secuencia que incluye no enteros, la clase cambia
+class(seq(1, 10, 0.5))
+
+#Crear Sequencias incremento 1
+seq(1, 10)
+
+#Crear Sequencia incremento de 2
+seq(1, 10, 2)
+
+
+
+##########SUBCONJUNTOS####################
+#cceso a elementos específicos de un vector
+codes[2];
+
+#obtener más de una entrada utilizando un vector de entradas
+codes[c(1,3)]
+
+#Obtener secuencia defeinida
+codes[1:2]
+
+#obtener valor por medio del nombre 
+codes["canada"]
+codes[c("egypt","italy")]
+
+
+########CONVERSION FORZADA##############
+x <- 1:5
+
+# 2Character
+y <- as.character(x)
+# 2Numeros
+as.numeric(y)
+
+#NA no avaliable por mala conversion
+x <- c("1", "b", "3")
+as.numeric(x)
+as.numeric(y)
